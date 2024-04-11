77bf05bf52b501b646b5074e2fa3c16e911901b6
anshul sangrame <anshul.sangrame@gmail.com>
2024-04-11 07:13:48 +0000

    pcap-arp

--- assgn7/get_pcap_files.sh
+++ assgn7/get_pcap_files.sh
@@ -4,3 +4,4 @@ cp /var/snap/lxd/common/lxd/storage-pools/default/containers/trudy1/rootfs/root/
 
 # cp /var/snap/lxd/common/lxd/storage-pools/default/containers/alice1/rootfs/root/capture-arp-alice.pcap pcaps/
 # cp /var/snap/lxd/common/lxd/storage-pools/default/containers/bob1/rootfs/root/capture-arp-bob.pcap pcaps/
+cp /var/snap/lxd/common/lxd/storage-pools/default/containers/trudy1/rootfs/root/capture-arp-trudy.pcap pcaps/
